d8f862a4fd312dc1cc8b3db7d31af54c02f4677d
Prabanjan Jeyasankar <prabanjanjeyasankar@gmail.com>
2025-12-28 02:22:41 +0530

    feat - add vercel-analytics for tracking.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 // import { StrictMode } from 'react'
 import Clarity from '@microsoft/clarity'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   <QueryClientProvider client={queryClient}>
     <App />
+    <Analytics />
   </QueryClientProvider>
   // </StrictMode>
 )
